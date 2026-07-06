df148caa25844c4cbd3edc219c7fb1751903a21a
Nowadays <Nowadays@DESKTOP-LPBAHN2>
2026-07-07 00:32:35 +0600

    Add task08 projects

---
+++ task08/CommandRunner/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.IO;
+using System.Reflection;
+using CommandLib;
+
+namespace CommandRunner
+{
+    class Program
+    {
+        static void Main()
+        {
+            string dllPath = Path.Combine(Directory.GetCurrentDirectory(), "..", "..", "..", "..",
+                                         "FileSystemCommands", "bin", "Debug", "net8.0", "FileSystemCommands.dll");
+
+            if (!File.Exists(dllPath))
+                return;
+
+            Assembly assembly = Assembly.LoadFrom(dllPath);
+
+            Type dirSizeType = assembly.GetType("FileSystemCommands.DirectorySizeCommand");
+            var dirSizeCommand = Activator.CreateInstance(dirSizeType, new object[] { Directory.GetCurrentDirectory() }) as ICommand;
+            dirSizeCommand?.Execute();
+
+            Type findFilesType = assembly.GetType("FileSystemCommands.FindFilesCommand");
+            var findFilesCommand = Activator.CreateInstance(findFilesType, new object[] { Directory.GetCurrentDirectory(), "*.cs" }) as ICommand;
+            findFilesCommand?.Execute();
+        }
+    }
+}
