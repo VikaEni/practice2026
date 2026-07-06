2e91cffe46793d31f25e21ab65bd5c674fc6d8a9
Nowadays <Nowadays@DESKTOP-LPBAHN2>
2026-07-06 22:54:00 +0600

    Add task04 projects

---
+++ task04/Cruiser.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using task04;
+
+namespace task04
+{
+    public class Cruiser : ISpaceship
+    {
+        private int _speed = 50;
+        private int _firePower = 100;
+        private int _currentAngle = 0;
+        public int Speed => _speed;
+        public int FirePower => _firePower;
+        public void MoveForward()
+        {
+        }
+        public void Rotate(int angle)
+        {
+            _currentAngle = (_currentAngle + angle) % 360;
+        }
+        public void Fire()
+        {
+        }
+    }
+}
---
+++ task04tests/SpaceshipTests.cs
@@ -0,0 +1,40 @@
+using task04;
+using Xunit;
+
+namespace task04tests
+{
+    public class SpaceshipTests
+    {
+        [Fact]
+        public void Cruiser_ShouldHaveCorrectStats()
+        {
+            ISpaceship cruiser = new Cruiser();
+            Assert.Equal(50, cruiser.Speed);
+            Assert.Equal(100, cruiser.FirePower);
+        }
+
+        [Fact]
+        public void Fighter_ShouldBeFasterThanCruiser()
+        {
+            var fighter = new Fighter();
+            var cruiser = new Cruiser();
+            Assert.True(fighter.Speed > cruiser.Speed);
+        }
+
+        [Fact]
+        public void Fighter_ShouldHaveCorrectStats()
+        {
+            ISpaceship fighter = new Fighter();
+            Assert.Equal(100, fighter.Speed);
+            Assert.Equal(50, fighter.FirePower);
+        }
+
+        [Fact]
+        public void Cruiser_ShouldHaveMoreFirePowerThanFighter()
+        {
+            var cruiser = new Cruiser();
+            var fighter = new Fighter();
+            Assert.True(cruiser.FirePower > fighter.FirePower);
+        }
+    }
+}
